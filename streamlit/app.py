3d611a242d193b9a0c58c32626054797aff29646
Vikash Yadav <incrediblevry0611@gmail.com>
2022-05-02 22:11:51 +0530

    added plots and predict button

--- streamlit/app.py
+++ streamlit/app.py
@@ -3,7 +3,9 @@ import sklearn
 import streamlit as st
 import numpy as np
 import librosa
-st.write("Speech Emotion Recognizer")
+import librosa.display
+import matplotlib.pyplot as plt
+st.write("SPEECH EMOTION RECOGNISER")
 uploaded_file = st.file_uploader("Choose an audio file")
 model=pickle.load(open("MLP.sav","rb"))
 scaler=pickle.load(open("scaler.sav","rb"))
@@ -19,16 +21,44 @@ emotions = {
 }
 
 if uploaded_file is not None:
-   bytes_data=uploaded_file.getvalue()
-   st.write("The uploaded audio file is:")
-   st.audio(bytes_data)
-   wave_,sr=librosa.load(uploaded_file)
-   mfcc=np.mean(librosa.feature.mfcc(wave_).T,axis=0)
-   X=[]
-   X.append(mfcc)
-   X=np.array(X)
-   X=scaler.transform(X)
-   preds=model.predict(X)
-   st.write("The emotion recognized is:")
-   st.write(emotions[preds[0]])
+    bytes_data=uploaded_file.getvalue()
+    st.write("The uploaded audio file is:")
+    st.audio(bytes_data)
+
+    wave_,sr=librosa.load(uploaded_file)
+
+    fig, ax = plt.subplots(1,2,figsize=(10, 2))
+    img1 = librosa.display.waveshow(wave_,sr, ax=ax[0], color='green')
+    ax[0].set_title("Waveform")
+    ax[0].set_xlabel("")
+    ax[0].set_ylabel("")
+    ax[0].set_xticks([])
+    ax[0].set_yticks([])
+
+    n_fft = 2048
+    hop_length = 512
+    mel_signal = librosa.feature.melspectrogram(y=wave_, sr=sr, hop_length=hop_length,n_fft=n_fft)
+    spectrogram = np.abs(mel_signal)
+    power_to_db = librosa.power_to_db(spectrogram, ref=np.max)
+    img2 = librosa.display.specshow(power_to_db, sr=sr, x_axis='time', y_axis='mel', cmap='magma', hop_length=hop_length)
+    # fig.colorbar(img2,ax=ax[1],label='dB')
+    ax[1].set_title("Mel Spectogram (in dB)")
+    ax[1].set_xlabel("")
+    ax[1].set_ylabel("")
+    ax[1].set_xticks([])
+    ax[1].set_yticks([])
+
+    fig.tight_layout()
+    st.pyplot(fig)
+
+
+    if st.button("Predict"):
+        mfcc=np.mean(librosa.feature.mfcc(wave_).T,axis=0)
+        X=[]
+        X.append(mfcc)
+        X=np.array(X)
+        X=scaler.transform(X)
+        preds=model.predict(X)
+        st.write("The emotion recognized is:")
+        st.write(emotions[preds[0]].upper())
 
